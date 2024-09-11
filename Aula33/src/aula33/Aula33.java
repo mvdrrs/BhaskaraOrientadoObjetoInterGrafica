3e73db3d21e5a1a9f0fe4e205114a043abd3ac8c
mvdrrs <mvdrrs2@gmail.com>
2024-09-11 10:58:24 -0300

    Add files via upload

---
+++ Aula33/src/aula33/Aula33.java
@@ -0,0 +1,26 @@
+
+package aula33;
+
+
+public class Aula33 {
+
+    public static void main(String[] args) {
+        
+        Bhaskara bas = new Bhaskara ();
+        bas.setA(2);
+        bas.setB(-3);
+        bas.setC(-5);
+        
+        System.out.println(bas.delta());
+        System.out.println(bas.x1());
+        System.out.println(bas.x2());
+        
+        
+        
+        
+               
+        
+        
+    }
+    
+}
